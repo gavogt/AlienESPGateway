56ed7e210172ef9474c1dde7572bd88cbe314ec1
Gabriel Vogt <gvogt@asu.edu>
2025-08-07 09:26:44 -0700

    feat(sidebar): add navigation with useNavigate for better route control

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';  
 import { FaSatellite, FaCogs, FaHeartbeat, FaBell } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import './Sidebar.css'; 
+import { useNavigate } from 'react-router-dom';
+import './Sidebar.css';
 
 export default function SidebarNav() {
+  const navigate = useNavigate();
+
   return (
     <Sidebar breakPoint="md">
       <Menu iconShape="circle">
-        <MenuItem icon={<FaSatellite />}><Link to="/">Telemetry</Link></MenuItem>
-        <MenuItem icon={<FaCogs />}><Link to="/control">Control</Link></MenuItem>
-        <SubMenu icon={<FaHeartbeat />} title="Diagnostics">
-          <MenuItem><Link to="/diagnostics/logs">Logs</Link></MenuItem>
-          <MenuItem icon={<FaBell />}><Link to="/alerts">Alerts</Link></MenuItem>
+        <MenuItem icon={<FaSatellite />} onClick={() => navigate('/')}>
+          Telemetry
+        </MenuItem>
+        <MenuItem icon={<FaCogs />} onClick={() => navigate('/control')}>
+          Control
+        </MenuItem>
+        <SubMenu icon={<FaHeartbeat />} label="Diagnostics">
+          <MenuItem onClick={() => navigate('/diagnostics/logs')}>
+            Logs
+          </MenuItem>
+          <MenuItem icon={<FaBell />} onClick={() => navigate('/alerts')}>
+            Alerts
+          </MenuItem>
         </SubMenu>
       </Menu>
     </Sidebar>
